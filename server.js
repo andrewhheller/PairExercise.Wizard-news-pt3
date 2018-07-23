2b25fa3adedb55d2150015eed76533d73f8f4652
Andrew Heller <andrewhheller@gmail.com>
2018-07-23 17:13:22 -0400

    modified env variable port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app.js');
 
 // ### SERVER LISTENING PORT ###
 
-const PORT = process.env.port || 1337;
+const PORT = process.env.PORT || 1337;
 
 app.listen(PORT, () => {
   console.log(`listening in port ${PORT}`);
